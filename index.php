877d44329ef510c07d7a248f61af4e4a9652b135
iphipan <50053458+iphipan@users.noreply.github.com>
2019-06-01 13:05:39 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 
+<?php get_header(); ?>
+	<div class="row">
+		<div class="col-sm-8 blog-main">
 
-      <div class="row">
+			<?php
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+        get_template_part( 'content', get_post_format() );
+			endwhile; endif;
+			?>
 
-        <div class="col-sm-8 blog-main">
+		</div> <!-- /.blog-main -->
 
-        </div><!-- /.blog-main -->
-
-
-      </div><!-- /.row -->
+		<?php get_sidebar(); ?>
+	</div> <!-- /.row -->
+<?php get_footer(); ?>
